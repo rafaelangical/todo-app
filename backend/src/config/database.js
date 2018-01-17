6b8990aaea639520c6bb5a9ff0c662d0071947aa
rafaelangical <rsnrafael@outlook.com>
2018-01-17 20:33:44 -0300

    Create backend of todo-app

---
+++ backend/src/config/database.js
@@ -0,0 +1,4 @@
+const mongoose =  require('mongoose')
+mongoose.Promise = global.Promise
+
+module.export = mongoose.connect('mongodb://localhost/todo')
---
+++ backend/src/config/routes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+module.exports = function(server){
+    
+    //ROTAS DA API
+    const router = express.Router()
+    server.use('/api', router)
+
+    //rotas todo    
+    const todoService = require('../api/todo/todoService')
+    todoService.register(router, '/todos')
+
+}
