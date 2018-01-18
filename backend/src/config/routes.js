e4cefb577272d2878ed05b5d2c1a8528ef62abf1
rafaelangical <rsnrafael@outlook.com>
2018-01-17 22:40:39 -0300

    Finalize backend and format identation

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,13 +1,11 @@
 const express = require('express')
 
-module.exports = function(server){
-    
-    //ROTAS DA API
-    const router = express.Router()
-    server.use('/api', router)
+module.exports = function(server){    
+  //ROTAS DA API
+  const router = express.Router()
+  server.use('/api', router)
 
-    //rotas todo    
-    const todoService = require('../api/todo/todoService')
-    todoService.register(router, '/todos')
-
+  //rotas todo    
+  const todoService = require('../api/todo/todoService')
+  todoService.register(router, '/todos')
 }
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,7 +2,6 @@ const port = 3003
 
 const bodyParser = require('body-parser')
 const express = require('express')
-
 const server = express()
 const allowCors = require('./cors')
 
