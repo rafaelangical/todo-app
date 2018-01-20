9b5726964c8dfa0c0cc2032dcb8e99bb88bb197c
rafaelangical <rsnrafael@outlook.com>
2018-01-19 21:17:23 -0300

    Add app.jsx

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,10 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/fonte-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+export default props =>(
+  <div className="container">
+    <h1></h1>
+  </div>
+)
