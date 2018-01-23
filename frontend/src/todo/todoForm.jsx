4caaace04f6e1bfdac3f3289987bd5ea9dea5288
rafaelangical <rsnrafael@outlook.com>
2018-01-22 23:19:29 -0300

    Update todoForm

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 export default props => (
-  <div>
-    <h1>Formulariio</h1>
+  <div role="form" className="todoForm">
+    <div className="col-xs-12 col-sm-9 col-md-10">
+      <input type="text" className="form-control" 
+      placeholder="Adicione uma tarefa"></input>
+    </div>
+
+    <div className="col-xs-12 col-sm-3 col-md-12">
+      <button className="btn btn-primary"><i className="fa fa-plus"></i></button>
+    </div>
   </div>
 )
