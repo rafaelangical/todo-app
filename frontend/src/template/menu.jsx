2058fd5e8bc0949b25d442eb2a335aa466ed5d6f
rafaelangical <rsnrafael@outlook.com>
2018-01-23 09:51:48 -0300

    Add import from font-awesome

--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../../node_modules/font-awesome/css/font-awesome.min.css'
 
 export default props => (
   <div className="navbar navbar-inverse bg-inverse">
