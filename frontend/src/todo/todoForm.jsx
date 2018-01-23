c83888366b116a6c12060dc2c721060a7e445d3c
rafaelangical <rsnrafael@outlook.com>
2018-01-23 10:31:29 -0300

    Changes in className

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default props => (
   <div role="form" className="todoForm">
-    <div className="col-xs-12 col-sm-9 col-md-10">
+    <div className="col-xs-12 col-sm-9 col-md-9">
       <input type="text" className="form-control" 
       placeholder="Adicione uma tarefa"></input>
     </div>
 
-    <div className="col-xs-12 col-sm-3 col-md-12">
+    <div className="col-xs-12 col-sm-3 col-md-3">
       <button className="btn btn-primary"><i className="fa fa-plus"></i></button>
     </div>
   </div>
