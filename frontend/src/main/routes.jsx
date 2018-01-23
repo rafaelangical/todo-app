25e8389667161aca0003796bff0986b604582880
rafaelangical <rsnrafael@outlook.com>
2018-01-23 10:16:24 -0300

    Remove menu rom route

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Todo from '../todo/todo'
 import About from '../about/about'
+import Menu from '../template/menu'
+import { Router, Route, Link, browserHistory } from 'react-router';
 
 export default props => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/todos" component={Todo} />
-      <Route path="/about" component={About} />
-    </Switch>
-  </BrowserRouter>
+  <Router history={browserHistory}>
+    <Route path='/todo' component={Todo} />
+    <Route path='/about' component={About} />
+  </Router>
 )
