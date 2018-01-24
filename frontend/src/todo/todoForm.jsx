4fae3e69355925e916b7d098bdc7f4a2d89ca69a
rafaelangical <rsnrafael@outlook.com>
2018-01-23 23:24:07 -0300

    Update

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import Grid from  '../template/grid'
+import IconButton from '../template/iconButton'
 
 export default props => (
   <div role="form" className="todoForm">
-    <Grid cols="12 9 10"
+    <Grid cols="12 9 10">
       <input type="text" className="form-control" 
       placeholder="Adicione uma tarefa"></input>
     </Grid>
 
     <Grid cols="12 3 2">
-      <button className="btn btn-primary">
-        <i className="fa fa-plus"></i>
-      </button>
+      <IconButton style="primary" icon="plus"></IconButton>
     </Grid>
   </div>
 )
