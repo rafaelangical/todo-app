918dde639250596d340a6b198095ee4df5b8fd5e
rafaelangical <rsnrafael@outlook.com>
2018-01-23 22:58:03 -0300

    Update iconButton and import component If

--- frontend/src/template/iconButton.jsx
+++ frontend/src/template/iconButton.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import If from './if'
 
-export defaul props => (
-  if(props.hide){
-    return null
-  }else{
-    return <button className={'btn btn-'+props.style } onClick={props.onClick}>
-      <i className={'fa fa-'+ props.icon}></i>
+export default props => (
+  <If test={!props.hide}>
+    <button className={'btn btn-'+props.style} onClick={props.onClick}>
+      <i className={'fa fa-'+props.icon}></i>
     </button>
-  }
+  </If>
 )
