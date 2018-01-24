d038c3b9b28ff70efee45570c9a8b8455836a814
rafaelangical <rsnrafael@outlook.com>
2018-01-23 23:31:35 -0300

    Create functions handleAdd

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,13 +4,18 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component{
+
+  handleAdd() {
+    console.log("Funcionou")
+  }
+
   render(){
     return(
       <div>
         <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-        <TodoForm />
+        <TodoForm handleAdd={this.handleAdd} />
         <TodoList />
-        </div>
+      </div>
     )
   }
 }
