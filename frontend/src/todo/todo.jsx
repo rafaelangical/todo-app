f3abc1859ee2256c850abcf0f5046d6fe89caf87
rafaelangical <rsnrafael@outlook.com>
2018-02-07 21:56:31 -0300

    Update

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -25,11 +25,12 @@ export default class Todo extends Component{
 
   refresh(description = '') {
     const search = description ? `&description__regex=/${description}/` : ''
-    axios.get(URL,`${URL}?Sort=-createdAt${search}`)
-      .then( resp => this.setState({...this.state, description: '', list: resp.data }))
+    axios.get(`${URL}?Sort=-createdAt${search}`)
+      .then( resp => this.setState({...this.state, description, list: resp.data }))
   }
 
   handleSearch(){
+    console.log("clicou")
     this.refresh(this.state.description)
   }
 
@@ -46,16 +47,16 @@ export default class Todo extends Component{
 
   handleRemove(todo){
     axios.delete(`${URL}/${todo._id}`)
-      .then(resp => this.refresh())
+      .then(resp => this.refresh(this.state.description))
   }
 
   handleMarkAsDone(todo){
     axios.put(`${URL}/${todo._id}`, { ...todo, done: true})
-      .then(resp => this.refresh())
+      .then(resp => this.refresh(this.state.description))
   }
   handleMarkAsPending(todo){
     axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
-      .then(resp => this.refresh())
+      .then(resp => this.refresh(this.state.description))
   }
 
 
