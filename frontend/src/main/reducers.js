257aba7b27142e0ef4150a7f25b0ef8cffb82d12
rafaelangical <rsnrafael@outlook.com>
2018-02-28 13:15:54 -0300

    Update

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux'
-
+import todoReducer from '../todo/todoReducer'
 const rootReducer = combineReducers({
-  todo: () => ()
+  todo: todoReducer
 })
 
 export default rootReducer
