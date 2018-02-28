a39d6927353e4dab8e7355b24289d518760bfed5
rafaelangical <rsnrafael@outlook.com>
2018-02-28 13:07:48 -0300

    Changes

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,22 +1,7 @@
 import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
-  todo: () => ({
-    description: 'Ler Livro',
-    list: [{
-      _id: 1,
-      description: 'Descricao 1',
-      done: true
-    }, {
-        _id: 2,
-        description: 'Descricao 2',
-        done: false
-    }, {
-        _id: 3,
-        description: 'Descricao 3',
-        done: false
-    }]
-  })
+  todo: () => ()
 })
 
 export default rootReducer
