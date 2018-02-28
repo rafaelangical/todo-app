ae7073587d10d816277f1d1fa00cb6a95768773e
rafaelangical <rsnrafael@outlook.com>
2018-02-28 12:55:55 -0300

    Changed handleChange to changeDescription

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -20,7 +20,7 @@ const TodoForm = props => {
       <Grid cols="12 9 10">
         <input id="description" className="form-control"
           placeholder="Adicione uma tarefa" value={props.description}
-          onChange={props.handleChange}
+          onChange={props.changeDescription}
           onKeyUp={keyHandler}>
         </input>
       </Grid>
