fb00c879160ccb14203ac1dbf876b133508be2a0
rafaelangical <rsnrafael@outlook.com>
2018-02-28 12:12:58 -0300

    Import reducers on index

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './main/app'
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
+import reducers from './main/reducers'
 
 ReactDOM.render(<App />, document.getElementById('app'))
