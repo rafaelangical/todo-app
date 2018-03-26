ccbf9cb8a45df10f59064227d88ab14dc031bdcf
rafaelangical <rsnrafael@outlook.com>
2018-03-26 19:52:14 -0300

    Create const devTools

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,7 +5,11 @@ import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducers from './main/reducers'
 
-const store = createStore(reducers)
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+&& window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(reducers, devTools)
 
 ReactDOM.render(
   <Provider store={store}>
