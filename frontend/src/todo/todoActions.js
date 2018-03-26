5e3d945158581f4a556f558487be40739780a298
rafaelangical <rsnrafael@outlook.com>
2018-03-26 20:03:42 -0300

    Created new action

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -6,3 +6,11 @@ export const changeDescription = event => ({
   type: 'DESCRIPTION_CHANGED',
   payload: event.target.value
 })
+
+export const seach = () => {
+  const request = axios.get(`${URL}?sort=-createdAt`)
+  return {
+    type: 'TODO_SEARCHED',
+    payload: request
+  }
+}
